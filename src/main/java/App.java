43d3686017bdee541ef120c0c5043509e98eb23a
Cergonchik <152849771+CergeyGoncharov@users.noreply.github.com>
2023-12-20 14:53:28 +0300

    Spring 1.0

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,13 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println("Являются ли bean одним объектом? " + (bean == bean2));
+
+        Cat cstBean1 = (Cat) applicationContext.getBean("cat");
+        Cat cstBean2 = (Cat) applicationContext.getBean("cat");
+        System.out.println("Являются ли catBean одним объектом? " + (cstBean1 == cstBean2));
+
     }
 }
